f3df2d320de85e2c299c691e36327996fc760d9b
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-17 23:40:31 -0300

    Stream http

--- stream_https.js
+++ stream_https.js
@@ -1,1 +1,16 @@
 const http = require('http')
+const {createRedSteam} = require('fs')
+
+const server = http.createServer((req, res) => {
+    const fileStream = createRedSteam('./data/bigfile.txt',{
+        encoding: 'utf-8'
+    })
+    fileStream.on('data',(chunk)=>{
+        fileStream.pipe(res)
+    })
+    fileStream.on('error',error =>{
+        console.log(error)
+    })
+})
+server.listen(3000)
+console.log('server on port ' + 3000)
